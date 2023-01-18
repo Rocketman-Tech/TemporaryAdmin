edfdade6de39c096acf66a85184e91975c2a0f89
Chad Lawson <cdlawson@coldshadow.com>
2023-01-18 14:37:25 -0600

    Complete overhaul for 2023
    This includes upgrades for Token Authentication in the Jamf API as well as fixes to the timing of log uploads and LaunchDaemon cleanup.

--- README.md
+++ README.md
@@ -13,10 +13,11 @@ It was, and remains, one of my favorite JNUC presentations. Andrina presented a 
 
 One of them included a common situation: all users have standard access but sometimes need elevated privileges. So that neither she nor her team would need to spend their days going around and typing in admin credentials for single use cases, she created the first version of this workflow. Now an end-user could go into Self Service and press a button granting them admin rights for a limited time.
 
-**Fun fact:** In her presentation she calls out and credits Kyle Brockman as a source for some of that workflow. I'm sitting next to Kyle (well one seat over) in that theater. Kyle and I had been co-workers before that, and I know that at least some of that workflow came from me! Funny how everything goes around like that.
+**Fun fact:** In her presentation she calls out and credits Kyle Brockman as a source for some of that workflow. I'm sitting next to Kyle (well one seat over) in that theater. Kyle and I had been co-workers before that, and I believe that at least some of that workflow came from me! Funny how everything goes around like that.
 
 ## How It Works
-This workflow is meant to be a policy in Self Service that is run on demand by the user, and gives them temporary admin access for a set amount of time. It can be customized to prompt the user for a reason they need admin access (which is saved in the Policy log), upload the system logs from the computer to the computer's inventory record in Jamf Pro (so there's a record of what they did while they had admin access), and only allow the policy to be run once upon IT request.  All of these options can be set by parameters. This allows for two main workflows (although more exist, since most parameters are optional):  
+
+This workflow is meant to be a policy in Self Service that is run on demand by the user, and gives them temporary admin access for a set amount of time. It can be customized to prompt the user for a reason they need admin access (which is saved in the Policy log), upload the system logs from the computer to the computer's inventory record in Jamf Pro (so there's a record of what happened on the computer while they had admin access), and only allow the policy to be run once upon IT request.  All of these options can be set by parameters. This allows for two main workflows (although more exist, since most parameters are optional):  
 
 **Workflow 1: Temporary Admin on Demand**
 - User opens Self Service and runs the Temporary Admin policy
@@ -32,45 +33,57 @@ This workflow is meant to be a policy in Self Service that is run on demand by t
 - A log is uploaded to the computer’s inventory record showing what the user did during their admin session
 - User is removed from the Temporary Admin Static Group and will need to make another request next time they need admin rights
 
-## Parameters
-- Parameter 4: The time that the admin rights will be set for, in minutes. Defaults to 5 minutes if not specified.
-	- Label: Time (in minutes) for admin rights
-	- Type: Integer
-	- Example: 15
-- Parameter 5: If true, the user will be prompted with an AppleScript dialog why they need admin rights and the reason will be echoed out to the policy log.
-	- Label: Ask for a reason (y/n)
-	- Type: Boolean (y/n)
-	- Example: y
-- (Optional) Parameter 6: This string will be used in an API call to file upload the logs at the end
-	- Label: API Basic Authentication
-	- Type: String (must be a base64 hash)
-	- Requirements: API User with the following permissions
-		- Computers - Create | Read | Update
-		- File Attachments - Create | Read | Update
-		- Static Group - Read | Update
-	- Instructions: In Terminal run "echo -n 'jamfapi:Jamf1234' | base64 | pbcopy"
-	- Example: RGlkIHlvdSByZWFsbHkgZGVjb2RlIHRoaXM/Cg==
-- Parameter 7: If yes, the system logs for the duration of elevated rights will be attached to the computer record in Jamf Pro
-	- Label: Upload log to Jamf Pro (y/n)
-	- Type: Boolean (y/n)
-	- Requirements: Parameter 6 (API Basic Authentication) must be set
-	- Example: y
-- (Optional) Parameter 8: The name of the static group to remove the computer from after use. This ensures the user can only run the policy upon request. 
-	- Label: Static Group to remove Computer from after use
-	- Type: String (must match static group name)
-	- Requirement 1: Parameter 6 (API Basic Authentication) must be set
-	- Requirement 2: Static Group with matching name required
-	- Requirement 3: Scope of the Policy must be set the the Static Group
-	- Example: Temporary Admin
+## Policy Parameters
+
+This script forgoes the use of position-specific parameters for a more traditional shell script argument (``argv``) approach.
+
+Meaning, you can set any/all of the below options by entering one each in any of the policy script parameter slots. For example, you can specify the amount of time in parameter 4 _**or**_ parameter 11 _**or**_ any one you choose. But you can only specify one option for parameter slot and it must be in the following format:
+
+``--timemin=15`` for key/value pairs<br/>
+``--askreason`` for those that just need to be switched on
+
+The options, along with the expected values and defaults are:
+
+### For the Self Service promote policy
+* **``--demotetrigger=[x]`` - REQUIRED - The custom trigger for the demote policy. If left off, the demote policy will not be called and admin rights will remain indefinitely.** 
+* ``--action=promote`` - Defaults to 'promote' so only used for clarity/documentation.
+* ``--timemin=[x]`` - Time (in minutes) that admin rights are granted. _Default is 5 minutes_.
+* ``--askreason`` - If set the user will be prompted to provide a reason they need the access which is available in the policy log.
+* ``--removegroup=[x]`` - The name of a Static Group to which the policy is scoped and computers should be removed after admin rights are granted.
+
+### For the demote policy
+* **``--action=demote`` - REQUIRED - Defaults to 'promote' so 'demote' must be specified.**
+* ``--uploadlog`` - If set, the system logs for the duration of time that admin rights were granted will be added to the computer's record as an attachment.
+
+### For both/either policies
+* ``--basicauth=[x]`` - Base64 encoded "user:pass" credentials for an api user _(see "Basic Auth" below)_ - REQUIRED for ``--removegroup`` and ``--uploadlog`` options.
+* ``--domain=[x]`` - Domain for options set in local or managed plists. Defaults to 'tech.rocketman.tempadmin'
+
+## Basic Auth
+If you will be using either the ``--removegroup`` or ``--uploadlog`` options, you must provide the base64 encoded credentials of a Jamf Pro user with the appropriate levels of access.
+
+To get this string, you can enter the following in the terminal:
+
+``echo -n 'user:password' | base64``
+
+where ``user`` and ``password`` are those of the API user.
+
+**NOTE:** It is _**strongly**_ recommended that you create a separate user for this with only the required access. _(See "Deployment Instructions" below)_
   
 ## Deployment Instructions
 This workflow must be created and deployed through Jamf Pro using the following steps: 
-- Add MakeMeAnAdmin.sh to Jamf Pro with the parameter labels above
+- Add Temporary Admin.sh to Jamf Pro
+- Create the two policies
+	1. A Self Service policy that runs with the script with the 'promote' action (see below)
+		- Trigger: Self Service
+		- Frequency: Ongoing
+		- Scope: _All Computers_ **or** a specified _Static Group_
+	2. A policy with a custom trigger (specified in first policy) that demotes the user after the specified amount of time
+		- Trigger: Custom
+		- Frequency: Ongoing
+		- Scope: _All Computers_
 - Optional: Create a Static Group to give users one-time access to the Temporary Admin Policy
 - Optional: Create an API User with the following permissions
 	- Computers - Create
 	- File Uploads - Create | Read | Update
 	- Static Group - Read | Update
-- Create a Policy deploying MakeMeAnAdmin.sh through Self Service with the parameters set above
-- Optional: Scope to the Temporary Admin static group you created 
-	- Note: you could also scope it to all users, all standard users, specific departments, or a combination of the temporary admin account and all of the above, utilizing the power of Smart Groups within Jamf Pro.
--- Temporary Admin.sh
+++ Temporary Admin.sh
@@ -1,191 +1,321 @@
-#!/bin/bash
+#!/bin/zsh
 
-: HEADER = <<'EOL'
+:<<HEADER
 ██████╗  ██████╗  ██████╗██╗  ██╗███████╗████████╗███╗   ███╗ █████╗ ███╗   ██╗
 ██╔══██╗██╔═══██╗██╔════╝██║ ██╔╝██╔════╝╚══██╔══╝████╗ ████║██╔══██╗████╗  ██║
 ██████╔╝██║   ██║██║     █████╔╝ █████╗     ██║   ██╔████╔██║███████║██╔██╗ ██║
 ██╔══██╗██║   ██║██║     ██╔═██╗ ██╔══╝     ██║   ██║╚██╔╝██║██╔══██║██║╚██╗██║
 ██║  ██║╚██████╔╝╚██████╗██║  ██╗███████╗   ██║   ██║ ╚═╝ ██║██║  ██║██║ ╚████║
 ╚═╝  ╚═╝ ╚═════╝  ╚═════╝╚═╝  ╚═╝╚══════╝   ╚═╝   ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝
+
            Name: Temporary Admin
     Description: Grants the active user temporary admin rights
      Created By: Chad Lawson
-        License: Copyright (c) 2022, Rocketman Management LLC. All rights reserved. Distributed under MIT License.
+        License: Copyright (c) 2023, Rocketman Management LLC. All rights reserved. Distributed under MIT License.
       More Info: For Documentation, Instructions and Latest Version, visit https://www.rocketman.tech/jamf-toolkit
-     Parameters: $1-$3 - Reserved by Jamf (Mount Point, Computer Name, Username)
-					$4 - Time (in minutes) for admin rights
-					$5 - Ask for reason (y/n) - uses Applescript
-					$6 - API user "hash" (optional)
-					$7 - Upload logs to Jamf (y/n)
-					$8 - Remove computer from group at run?
-EOL
 
+      Parameter Options
+        A number of options can be set with policy parameters.
+        The order does not matter, but they must be written in this format:
+           --options=value
+           --trueoption
+        See the section immediately below starting with CONFIG for the list.
+HEADER
 
-###
-### Parameters and Globals
-###
+## Configuration Options and Defaults
+## An empty value indicates false or no default.
+declare -A CONFIG
+CONFIG=(
+	[timemin]=5                         # Time (in min) for admin rights
+	[askreason]=''                      # Ask user for reason - uses Applescript
+	[uploadlog]=''                      # Upload logs to Jamf (y/n)
+	[removegroup]=''                    # Name of static group from which to
+	[basicauth]=''                      # Base 64 encoded "user:pass" for an api
+	[action]="promote"                  # Alternative is "demote"
+	[demotetrigger]="demote"            # Custom trigger for demotion policy
+	[domain]="tech.rocketman.tempadmin" # plist(s) to read or store data and options
+)
 
-STATUS=0 ## default
+function loadPlist() {
+	local hashName=$1
+	local configFile=$2
 
-## Get the current user the Rocketman way
-CURRENTUSER=$([ $3 ] && echo "$3" || defaults read /Library/Preferences/com.apple.loginwindow lastUserName)
-
-## $4 = Time (in minutes) for admin rights
-TIMEMIN=$( [ $4 ] && echo $4 || echo "5" ) ## defaults to 5 minutes if not specified
-TIMESEC=$(( ${TIMEMIN} * 60 ))
-
-## $5 = Ask for reason (y/n)
-## If true, the user will be prompted with an AppleScript dialog why they need admin rights and the reason
-## will be echoed out to the policy log.
-ASKREASON=$5
-
-## $6 = API user hash*
-## 	*hash = base64 encoded string of 'user:password' for an API user for the next two options
-##		Note: Yes, I know this isn't a 'hash' but the word is more concise
-## 	If provided, this string will be used in an API call to file upload the logs at the end
-APIHASH=$6
-if [[ ${APIHASH} ]]; then
-	JSSURL=$(defaults read /Library/Preferences/com.jamfsoftware.jamf.plist jss_url)
-	SERIAL=$(system_profiler SPHardwareDataType | awk '/Serial Number/ {print $NF}')
-	RECORD=$(curl -s -H "Authorization: Basic ${APIHASH}" -H "Accept: text/xml" "${JSSURL}/JSSResource/computers/serialnumber/${SERIAL}")
-	COMPID=$(echo "${RECORD}" | xmllint --xpath '/computer/general/id/text()' -)
-fi
-
-## $7 = Upload logs to Jamf (y/n)
-## If yes, the system logs for the duration of elevated rights will be attached to the computer record in Jamf
-## Note: The API user 'hash' above must be provided above and that user must have the following permissions:
-##		Computers - Create
-##		File Uploads - Create Read Update
-UPLOADLOG=$7
-if [[ ${UPLOADLOG} == "y" && "${APIHASH}" ]]; then
-	TIMESTAMP=$(date +'%Y%m%d-%H%M%S')
-    LOGUPLOAD="log collect --last ${TIMEMIN}m --output /private/var/userToRemove/${CURRENTUSER}.logarchive ; zip -rm /private/var/userToRemove/${CURRENTUSER}.logarchive-${TIMESTAMP}.zip /private/var/userToRemove/${CURRENTUSER}.logarchive ; curl -s -H 'Authorization: Basic ${APIHASH}' '${JSSURL}/JSSResource/fileuploads/computers/id/${COMPID}' -F name=@'/private/var/userToRemove/${CURRENTUSER}.logarchive-${TIMESTAMP}.zip' -X POST"
-	LOGCOMMAND=$(echo $LOGUPLOAD | sed -e 's:/:\\/:g') ## To properly escape the slashes
-	echo "LOGGER=\"${LOGCOMMAND}\""
-fi
-
-## $8 = Remove computer from group after granting access?
-## If this parameter exists, it must be the name of a static computer group.
-## Note: The API user 'hash' above must be provided above and that user must have the following permissions:
-##		Computers - Read
-##		Static Computer Groups - Read Update
-## After the user is promoted, the computer will be removed from the static group preventing multiple uses
-STATICGROUP=$8
-ENCGROUP=$(curl -s -o /dev/null -w %{url_effective} --get --data-urlencode "${STATICGROUP}" "" | cut -c3-) ## URL Encoded
-if [[ ${STATICGROUP} ]]; then
-	XML="<?xml version=\"1.0\" encoding=\"UTF-8\" ?><computer_group><computer_deletions><computer><serial_number>${SERIAL}</serial_number></computer></computer_deletions></computer_group>"
-	curl -k -H "Content-type: text/xml" -H "Authorization: Basic ${APIHASH}" "${JSSURL}/JSSResource/computergroups/name/${ENCGROUP}" -d "${XML}" -X PUT
-fi
-
-## Allow for overrides of everything so far...
-## If the 11th policy parameter contains an equal sign, run eval on the
-## whole thing.
-## Example: If $11 is 'NUM=5;HIDDENFLAG=;FORCE=1;STORELOCAL="Yes"', then
-##  the values of the variables with the same name of those above would change.
-## WARNING! This would be HORRIBLE security in a script that remains local
-##          as any bash-savvy user could inject whatever code they wanted to.
-##          This danger is LESSENED by the fact that the parameters are
-##          provided at run-time by Jamf and the script is not stored on
-##          the computer outside the policy run.
-[[ "${11}" == *"="* ]] && eval ${11} ## Comment out to disable
-
-###
-### Functions
-###
-
-## Hopefully you never need this
-function debugLog () {
-	local logFile=$1
-	local logText=$2
-	local timeStamp=$(date +'%Y-%m-%d %H:%M:%S')
-	echo "${timeStamp}: ${logText}" >> ${logFile}
+	if [[ -f "${configFile}" ]]; then
+		echo "Updating $hashName with $configFile"
+		for key in ${(Pk)hashName}; do
+			val=$(defaults read "${configFile}" "${key}" 2>/dev/null)
+			if [[ $? -eq 0 ]]; then
+				echo "UPDATE: Setting ${key} to ${val}"
+				eval "${hashName}[$key]=$val"
+			fi
+		done
+	fi
 }
+
+function loadArgs() {
+	hashName=$1
+	shift ## Now the rest of the arguments start at 1
+
+	## Parsing command line arguments
+	if [[ $1 == "/" ]]; then ## We are in a Jamf environment
+		eval "${hashName}[currentuser]=$3"
+		shift 3
+	fi
+	while [[ $1 ]] ; do
+		case $1 in
+			--*=* ) # Key/Value pairs
+				key=$(echo "$1" | sed -re 's|^\-\-([^=]+)\=.*$|\1|g')
+				val=$(echo "$1" | sed -re 's|^\-\-[^=]+\=(.*)$|\1|g')
+			;;
+	
+			--* ) # Simple flags
+				key=$(echo "$1" | sed -re 's|\-+(.*)|\1|g')
+				val="True"
+			;;
+		esac
+		keys=${(Pk)hashName}
+		if (($keys[(Ie)$key])); then
+			echo "${key} is valid. Setting to ${val}"
+			eval "${hashName}[${key}]=${val}"
+		else
+			echo "Ignoring unknown key: ${key}"
+		fi
+	shift
+	done
+}
+
+function getAPIToken() {
+	jamfURL=$1
+	basicAuth=$2
+
+	authToken=$(curl -s \
+		--request POST \
+		--url "${jamfURL}/api/v1/auth/token" \
+		--header "Accept: application/json" \
+		--header "Authorization: Basic ${basicAuth}" \
+		2>/dev/null \
+	)
+	
+	## Courtesy of Der Flounder
+	## Source: https://derflounder.wordpress.com/2021/12/10/obtaining-checking-and-renewing-bearer-tokens-for-the-jamf-pro-api/
+	if [[ $(/usr/bin/sw_vers -productVersion | awk -F . '{print $1}') -lt 12 ]]; then
+		api_token=$(/usr/bin/awk -F \" 'NR==2{print $4}' <<< "$authToken" | /usr/bin/xargs)
+	else
+		api_token=$(/usr/bin/plutil -extract token raw -o - - <<< "$authToken")
+	fi
+	
+	echo ${api_token}
+}
+
+###
+### Parsing plists and argv
+###
+
+LOCALPLIST="/Library/Preferences/${CONFIG[domain]}.plist"
+PROFILE="/Library/Managed Preferences/${CONFIG[domain]}.plist"
+
+loadArgs  "CONFIG" ${argv} ## Start here to get changes in $CONFIG[domain] for plists
+loadPlist "CONFIG" "${LOCALPLIST}"
+loadPlist "CONFIG" "${PROFILE}"
+loadArgs  "CONFIG" ${argv} ## Now take these as written in stone
+
+## Convert CONFIG key/value pair into a global (ALL CAPS)
+## Example: ${CONFIG[timemin]} becomes ${TIMEMIN}
+for key in ${(k)CONFIG}; do
+	GLOBAL=$(echo $key | tr '[:lower:]' '[:upper:]')
+	: ${(P)GLOBAL::=${CONFIG[$key]}}
+done
 
 ###
 ### MAIN
 ###
 
-##
-## Make sure they aren't already an admin first.
-##
-isAdmin=$(dseditgroup -o checkmember -m ${CURRENTUSER} admin | awk '{print $1}')
-if [[ $isAdmin == "yes" ]]; then
-	osascript -e 'display dialog "You are already an admin."'
-	exit 0
-else
-	if [[ ${ASKREASON} == "y" ]]; then
-		while [[ ${REASON} == "" ]]; do
-			REASON=$(osascript -e 'return the text returned  of (display dialog "Please state briefly why you need admin rights." default answer "")')
-		done
+if [[ ${ACTION} == "promote" ]]; then	
+
+	## Make sure they aren't already an admin first.
+	isAdmin=$(dseditgroup -o checkmember -m ${CURRENTUSER} admin | awk '{print $1}')
+	if [[ $isAdmin == "no" ]]; then
+		
+		## Okay. Do we need to ask them why they need this?
+		if [[ ${ASKREASON} ]]; then
+			while [[ ! ${REASON} ]]; do
+				## TODO: In the next version, all display text will exist within its own associative array
+				REASON=$(osascript -e 'return the text returned  of (display dialog "Please state briefly why you need admin rights." default answer "")')
+			done
+		fi
+		
+		## Prep the demote system
+		
+		## Store the name of the promoted user for the demote policy
+		defaults write "${LOCALPLIST}" userToRemove -string "${CURRENTUSER}"
+		defaults write "${LOCALPLIST}" promotedOn -string "$(date "+%Y-%m-%d %H:%M:%S")"
+		
+		## Create the LaunchDaemon to demote
+		launchDaemon="/Library/LaunchDaemons/${DOMAIN}.plist"
+		## Write it
+		defaults write "${launchDaemon}" Label -string "${DOMAIN}"
+		defaults write "${launchDaemon}" StartInterval -integer $((TIMEMIN*60))
+		defaults write "${launchDaemon}" UserName -string "root"
+		defaults write "${launchDaemon}" ProgramArguments -array
+		defaults write "${launchDaemon}" ProgramArguments -array-add "/usr/local/jamf/bin/jamf"
+		defaults write "${launchDaemon}" ProgramArguments -array-add "policy"
+		defaults write "${launchDaemon}" ProgramArguments -array-add "-event"
+		defaults write "${launchDaemon}" ProgramArguments -array-add "${DEMOTETRIGGER}"
+		defaults write "${launchDaemon}" StandardErrorPath -string "/tmp/stderr.log"
+		defaults write "${launchDaemon}" StandardOutPath -string "/tmp/stdout.log"
+		## Set it
+		chown root:wheel "${launchDaemon}"
+		chmod 644 "${launchDaemon}"
+		## Load it
+		launchctl load "${launchDaemon}"
+		
+		## Promote the user
+		echo "Granting ${CURRENTUSER} admin rights for ${TIMEMIN} minutes."
+		if [[ ${ASKREASON} == "y" ]]; then
+			echo "The reason they gave was: ${REASON}"
+		fi
+		dseditgroup -o edit -a ${CURRENTUSER} -t user admin
+		STATUS=$?
+		
+		## Let's double check our work and report error if not
+		verify=$(dseditgroup -o checkmember -m ${CURRENTUSER} admin)
+		if [[ ${verify} =~ "yes" ]]; then
+			echo "VERIFIED: ${verify}"
+			## TODO: In the next version, all display text will exist within its own associative array
+				endTime=$(date -v +${TIMEMIN}M +"%H:%M")
+				osascript -e "display dialog \"You will have administrative rights until ${endTime}. DO NOT ABUSE THIS PRIVILEGE!\" buttons {\"I agree\"} default button 1" &
+		else
+			echo "ERROR: ${verify}"
+			STATUS=1
+		fi
+		
+		## Remove from scoped static group if requested
+		if [[ ${REMOVEGROUP} ]]; then
+			if [[ ${BASICAUTH} ]]; then
+				## Get the Jamf Pro URL
+				jamfURL=$(defaults read /Library/Preferences/com.jamfsoftware.jamf.plist jss_url)
+				
+				## Token time
+				authToken=$(getAPIToken "${jamfURL}" "${BASICAUTH}")
+				
+				## Let's remove the computer from the requested group
+				serialNumber=$(system_profiler SPHardwareDataType | awk '/Serial Number/ {print $NF}')
+
+				## We need to url encode the group name for the XML
+				sanitiezedGroup=$(curl -s -o /dev/null -w %{url_effective} --get --data-urlencode "${REMOVEGROUP}" "" | cut -c3-) ## URL Encoded
+				dataXML="<?xml version=\"1.0\" encoding=\"UTF-8\" ?><computer_group><computer_deletions><computer><serial_number>${serialNumber}</serial_number></computer></computer_deletions></computer_group>"
+				## Send the API command
+				curl -s -X PUT \
+					-H "Content-type: text/xml" \
+					-H "Authorization: Bearer ${authToken}" \
+					"${jamfURL}/JSSResource/computergroups/name/${sanitiezedGroup}" \
+					-d "${dataXML}"
+
+				
+			else
+				echo "ERROR: API access requested but no auth provided"
+			fi
+		fi
+
+	else ## Nothing to do here
+		osascript -e 'display dialog "You are already an admin."'
+		echo "${CURRENTUSER} is already an admin. Exiting."
+		exit 0
 	fi
-	osascript -e "display dialog \"You will have administrative rights for ${TIMEMIN} minutes. DO NOT ABUSE THIS PRIVILEGE!\" buttons {\"I agree\"} default button 1"
-fi
+	
+else ## We are demoting the previously promoted user
+	
+	CURRENTUSER=$(defaults read "${LOCALPLIST}" userToRemove 2>/dev/null)
+	echo "Demoting ${CURRENTUSER}"
+	
+	## Demote the user
+	dseditgroup -o edit -d ${CURRENTUSER} -t user admin
 
-##
-## Create Cleanup Pieces
-##
+	## Verify demotion
+	verify=$(dseditgroup -o checkmember -m ${CURRENTUSER} admin)
+	if [[ ${verify} =~ "no" ]]; then
+		## No means they are NOT an admin. It worked.
+		echo "${CURRENTUSER} has been demoted."
 
-## Create the file to track the promoted user
-mkdir -p /private/var/userToRemove
-echo ${CURRENTUSER} >> /private/var/userToRemove/user
+		## Log it
+		defaults write "${LOCALPLIST}" demotedOn -string "$(date '+%Y-%m-%d %H:%M:%S')"
 
-## Create the script to demote with the launch daemon
-cat << 'EOF' > /Library/Application\ Support/JAMF/removeAdmin.sh
-if [[ -f /private/var/userToRemove/user ]]; then
+		## Upload Logs as Needed
+		if [[ ${UPLOADLOG} ]]; then
+			if [[ ${BASICAUTH} ]]; then
+				jamfURL=$(defaults read /Library/Preferences/com.jamfsoftware.jamf.plist jss_url)
+						
+				## Token time
+				authToken=$(getAPIToken "${jamfURL}" "${BASICAUTH}")
+		
+				## We need the computer's ID
+				serialNumber=$(system_profiler SPHardwareDataType | awk '/Serial Number/ {print $NF}')
+				compRecord=$(curl -s \
+					-H "Authorization: Bearer ${authToken}" \
+					-H "Accept: text/xml" \
+					"${jamfURL}/JSSResource/computers/serialnumber/${serialNumber}")
+				compID=$(echo "${compRecord}" | xmllint --xpath '/computer/general/id/text()' -)
+				
+				## Gather the logs
+				startTime=$(defaults read "${LOCALPLIST}" promotedOn 2>/dev/null)
+				if [[ $? -gt 0 ]]; then
+					startTime=$(date '+%Y-%m-%d %H:%M:%S')
+				fi
+				startString=$(echo ${startTime} | tr ' ' '-')
+				logArchive="/tmp/${CURRENTUSER}.logarchive"
+				logZip="/tmp/${CURRENTUSER}-${startTime}.logarchive.zip"
 
-	userToRemove=$(cat /private/var/userToRemove/user)
-	/usr/sbin/dseditgroup -o edit -d $userToRemove -t user admin
-
-	## If the logging option is selected, the line below gets swapped with that code
-	#LOGGING#
-
-	rm /Library/LaunchDaemons/removeAdmin.plist
-	rm "/Library/Application Support/JAMF/removeAdmin.sh"
-	rm -rf /private/var/userToRemove
-
-	launchctl unload /Library/LaunchDaemons/removeAdmin.plist
-fi
+				## Note: have to use the full path here because 'log' is 
+				## also a built-in shell command.
+				echo "Getting the logs"
+				/usr/bin/log collect --output "${logArchive}" --last "${TIMEMIN}m" >/dev/null
+				echo "Compressing logs"
+				zip -rm "${logZip}" "${logArchive}" >/dev/null
+	
+				## Upload the Logs
+				echo "Uploading logs"
+				curl -s -X POST \
+					-H "Authorization: Bearer ${authToken}" \
+					"${jamfURL}/JSSResource/fileuploads/computers/id/${compID}" \
+					-F name=@"${logZip}"
+	
+			else
+				echo "ERROR: Log upload was requested by API credentials are missing."
+			fi
+		fi
+		
+	fi
+	##
+	## Remove LaunchDaemon
+	##
+	launchDaemon="/Library/LaunchDaemons/${DOMAIN}.plist"
+	cleanUpScript="/tmp/cleanup.sh"
+	cat << EOF > ${cleanUpScript}
+		#!/bin/zsh
+		TRAPEXIT() {
+			rm -rf "${launchDaemon}"
+			rm "${cleanUpScript}"
+		}
+	
+		TRAPTERM() {
+			waiting='False'
+		}
+	
+		waiting='True'
+		while [[ \${waiting} == 'True' ]]; do
+			sleep 1
+		done
+		launchctl unload "${launchDaemon}"
+		exit
 EOF
 
-## If the logging option is selected, this swaps out the block above with the code listed in the
-## parameter section for logging.
-if [[ ${UPLOADLOG} == "y" ]]; then
-	echo "LOGCOMMAND=\"${LOGCOMMAND}\""
-    sed -i '' -e "s/#LOGGING#/${LOGCOMMAND}/" "/Library/Application Support/JAMF/removeAdmin.sh"
+	## Now we put the cleanup script in the background
+	## This allows the Jamf Policy log to continue and send results to Jamf.
+	## After that happens, the Jamf Policy calls sends the TERM signal to 
+	## the cleanup script which finishes.
+	## And when the cleanup script unloads the LaunchDaemon process, it gets 
+	## sent the EXIT signal which causes the LaunchDaemon plist and cleanup
+	## script to get removed.
+	/bin/zsh ${cleanUpScript} &
 fi
 
-##
-## Create the plist
-##
-sudo defaults write /Library/LaunchDaemons/removeAdmin.plist Label -string "removeAdmin"
-sudo defaults write /Library/LaunchDaemons/removeAdmin.plist ProgramArguments -array -string /bin/sh -string "/Library/Application Support/JAMF/removeAdmin.sh"
-sudo defaults write /Library/LaunchDaemons/removeAdmin.plist RunAtLoad -boolean yes
-sudo defaults write /Library/LaunchDaemons/removeAdmin.plist StartInterval -integer ${TIMESEC}
-
-#Set ownership
-sudo chown root:wheel /Library/LaunchDaemons/removeAdmin.plist
-sudo chmod 644 /Library/LaunchDaemons/removeAdmin.plist
-
-#Load the daemon
-launchctl load /Library/LaunchDaemons/removeAdmin.plist
-
-##
-## Give the user admin rights
-##
-echo "Granting ${CURRENTUSER} admin rights for ${TIMEMIN} minutes."
-if [[ ${ASKREASON} == "y" ]]; then
-	echo "The reason they gave was: ${REASON}"
-fi
-dseditgroup -o edit -a ${CURRENTUSER} -t user admin
-STATUS=$?
-
-## Let's double check our work and report error if not
-VERIFY=$(dseditgroup -o checkmember -m ${CURRENTUSER} admin)
-if [[ ${VERIFY} =~ "yes" ]]; then
-	echo "VERIFIED: ${VERIFY}"
-else
-	echo "ERROR: ${VERIFY}"
-	STATUS=1
-fi
-
-exit ${STATUS}
